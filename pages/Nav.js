cbe49ed94e467390dd3d63a0f5da4d3602a4536f
Martin Molvær <42579391+martinmolvaer@users.noreply.github.com>
2022-03-10 16:53:56 +0100

    add github link

---
+++ pages/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <a href="https://github.com/martinmolvaer">
+        <p className="nav-item">Github</p>
+      </a>
+    </div>
+  );
+};
+
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
+import Nav from "./Nav";
+
 export default function Home() {
   return (
-    <div className="home">
-      <div>
-        <h1 className="title">Martin M.</h1>
-        <a href="mailto:martin@molvaer.no">
-          <span className="mail">martin.molvaer.no</span>
-        </a>
+    <div>
+      <div className="nav-wrapper">
+        <Nav />
+      </div>
+      <div className="home">
+        <div className="container">
+          <div className="head">
+            <h1 className="title">Martin M.</h1>
+            <a href="mailto:martin@molvaer.no">
+              <span className="mail">martin.molvaer.no</span>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
