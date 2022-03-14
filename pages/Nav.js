f0ca38a998808df1cbe88ee11c1adf3cd500e510
Martin Molvær <42579391+martinmolvaer@users.noreply.github.com>
2022-03-14 21:52:15 +0100

    add tailwind css + add darkmode-togle

--- pages/Nav.js
+++ pages/Nav.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Nav = () => {
+  const [darkMode, setDarkMode] = useState("");
+
+  const clickDarkMode = () => {
+    setDarkMode("dark");
+    console.log("clik");
+  };
+
   return (
-    <div className="nav">
+    <div className={`nav  ${darkMode}`}>
       <a href="https://github.com/martinmolvaer">
-        <p className="nav-item">Github</p>
+        <p className={`nav-item ${darkMode}`}>Github</p>
       </a>
+      <p onClick={clickDarkMode} className={`nav-item`}>
+        🌖
+      </p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,36 @@
-import Nav from "./Nav";
+import { useState } from "react/cjs/react.development";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState("");
+  const [showIcon, setShowIcon] = useState(false);
+
+  const getDark = () => {
+    setDarkMode("bg-black text-white");
+    setShowIcon(true);
+  };
+  const setLight = () => {
+    setDarkMode("");
+    setShowIcon(false);
+  };
+
   return (
-    <div>
-      <div className="nav-wrapper">
-        <Nav />
-      </div>
-      <div className="home">
-        <div className="container">
-          <div className="head">
-            <h1 className="title">Martin M.</h1>
-            <a href="mailto:martin@molvaer.no">
-              <span className="mail">martin.molvaer.no</span>
-            </a>
+    <div className={`flex h-screen items-center justify-center ${darkMode}`}>
+      <div>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold ">Martin M.</h1>
+          <a href="mailto:martin@molvaer.no">
+            <span className="items-center">martin.molvaer.no</span>
+          </a>
+          <div className="flex">
+            {showIcon ? (
+              <p className="cursor-pointer" onClick={setLight}>
+                ☀️
+              </p>
+            ) : (
+              <p className="cursor-pointer" onClick={getDark}>
+                🌙
+              </p>
+            )}
           </div>
         </div>
       </div>
