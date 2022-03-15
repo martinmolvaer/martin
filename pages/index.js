8ef782a9dc80149ace20321017b5ae5bb3501503
Martin Molvær <42579391+martinmolvaer@users.noreply.github.com>
2022-03-15 17:16:34 +0100

    add link to rull.molvaer

--- pages/index.js
+++ pages/index.js
@@ -14,23 +14,38 @@ export default function Home() {
 
   return (
     <div className={`flex h-screen items-center justify-center ${darkMode}`}>
+      <div className="flex absolute w-full top-0">
+        <div className="flex justify-center w-full">
+          <div className="p-4">
+            <a href="http://rull.molvaer.no">
+              <p className="text-2xl hover:scale-125 ease-in-out duration-300">
+                🎲
+              </p>
+            </a>
+          </div>
+          {showIcon ? (
+            <p
+              className="p-4 cursor-pointer text-2xl hover:scale-125 ease-in-out duration-300"
+              onClick={setLight}
+            >
+              ☀️
+            </p>
+          ) : (
+            <p
+              className=" p-4 cursor-pointer text-2xl hover:scale-125 ease-in-out duration-300"
+              onClick={getDark}
+            >
+              🌙
+            </p>
+          )}
+        </div>
+      </div>
       <div>
         <div className="text-center">
           <h1 className="text-4xl font-bold ">Martin M.</h1>
           <a href="mailto:martin@molvaer.no">
             <span className="items-center">martin.molvaer.no</span>
           </a>
-          <div className="flex justify-center">
-            {showIcon ? (
-              <p className="cursor-pointer" onClick={setLight}>
-                ☀️
-              </p>
-            ) : (
-              <p className="cursor-pointer" onClick={getDark}>
-                🌙
-              </p>
-            )}
-          </div>
         </div>
       </div>
     </div>
