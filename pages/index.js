ce290d8fa1ef0bff8706200851f61ec4f41c2402
Martin Molvær <42579391+martinmolvaer@users.noreply.github.com>
2022-03-19 20:06:02 +0100

    add hover-effect

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function Home() {
   const [darkMode, setDarkMode] = useState("");
   const [showIcon, setShowIcon] = useState(false);
+  const [weird, setWeird] = useState("");
+
+  console.log("hei");
 
   const getDark = () => {
     setDarkMode("bg-black text-white");
@@ -12,13 +15,32 @@ export default function Home() {
     setShowIcon(false);
   };
 
+  function getRandomInt(max) {
+    const randomNumber = Math.floor(Math.random() * max);
+
+    if (randomNumber === 0) {
+      weirdFunc();
+    }
+  }
+
+  useEffect(() => {
+    getRandomInt(10);
+  }, []);
+
+  const weirdFunc = () => {
+    setWeird("rotate-180");
+  };
+  const removeText = () => {
+    setWeird("");
+  };
+
   return (
     <div className={`flex h-screen items-center justify-center ${darkMode}`}>
       <div className="flex absolute w-full top-0">
         <div className="flex justify-center w-full">
           <div className="p-4">
             <a href="http://rull.molvaer.no">
-              <p className="text-2xl hover:scale-125 ease-in-out duration-300">
+              <p className="text-2xl hover:rotate-180 ease-in-out duration-300">
                 🎲
               </p>
             </a>
@@ -44,7 +66,13 @@ export default function Home() {
         <div className="text-center">
           <h1 className="text-4xl font-bold ">Martin M.</h1>
           <a href="mailto:martin@molvaer.no">
-            <span className="items-center">martin.molvaer.no</span>
+            <div
+              onMouseEnter={() => getRandomInt(10)}
+              onMouseLeave={removeText}
+              className={`p-3 text-white ${weird}  bg-black rounded-md hover:scale-105 ease-in-out duration-300`}
+            >
+              Say hi
+            </div>
           </a>
         </div>
       </div>
