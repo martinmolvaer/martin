793f0d2e83712987abad0d4d9c68f915528c9eb4
M51dev <107985281+M51dev@users.noreply.github.com>
2022-08-28 15:50:03 +0200

    remove

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [weird, setWeird] = useState("");
+  const [weird, setWeird] = useState('');
 
-  console.log("heisann");
-
-  function getRandomInt(max) {
-    const randomNumber = Math.floor(Math.random() * max);
-    if (randomNumber === 0) {
-      weirdFunc();
-    }
-  }
+  console.log('heisann');
 
   useEffect(() => {
-    document.title = "Molvaer.no";
+    document.title = 'Molvaer.no';
   });
 
-  const weirdFunc = () => {
-    setWeird("rotate-180");
-  };
-  const removeText = () => {
-    setWeird("");
-  };
-
   return (
-    <div className={`flex h-screen items-center justify-center `}>
-      <img
-        className="w-12 top-0 absolute left-0 p-4"
-        src="https://i.imgur.com/rz2KKCT.png"
-      ></img>
-
-      <div className="flex absolute w-full top-0">
-        <div className="flex justify-center w-full">
-          <div className="p-4">
-            <a href="http://rull.molvaer.no">
-              <p className="text-2xl hover:rotate-180 ease-in-out duration-300">
-                🎲
-              </p>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="text-center">
-          <h1 className="text-4xl font-bold ">Martin Molvaer.</h1>
-          <div className="flex justify-around mt-5">
-            <a href="mailto:martin@molvaer.no">
-              <div
-                onMouseEnter={() => getRandomInt(10)}
-                onMouseLeave={removeText}
-                className={`p-3 bg-black ${weird}  text-white rounded-md hover:scale-105 ease-in-out duration-300`}
-              >
-                Say hi
-              </div>
-            </a>
-            <a href="https://github.com/martinmolvaer">
-              <div
-                className={`p-3 bg-black text-white rounded-md hover:scale-105 ease-in-out duration-300`}
-              >
-                Github
-              </div>
-            </a>
-          </div>
-        </div>
+    <div>
+      <div className="h-full align-middle items-center w-full ">
+        <div class="box"></div>
       </div>
     </div>
   );
